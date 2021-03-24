c116e353e7f774a5a70a7ff97e66277b6fca9a69
VladislavHarkun <63816951+VladislavHarkun@users.noreply.github.com>
2021-03-24 20:12:26 +0300

    Delete src/main directory

--- src/main/java/by/tc/carrental/model/dao/UserDAO.java
+++
@@ -1,8 +0,0 @@
-package by.tc.carrental.model.dao;
-
-import by.tc.carrental.model.entity.User;
-
-public interface UserDAO {
-    public boolean registration(User user) throws DAOException;
-    public User authorization(User user) throws DAOException;
-}
--- src/main/java/by/tc/carrental/model/service/validation/UserValidator.java
+++
@@ -1,9 +0,0 @@
-package by.tc.carrental.model.service.validation;
-
-import by.tc.carrental.model.entity.User;
-import by.tc.carrental.model.service.ServiceException;
-
-public interface UserValidator {
-    public boolean checkAuthData(User user) throws ServiceException;
-    public boolean checkRegData(User user) throws ServiceException;
-}
